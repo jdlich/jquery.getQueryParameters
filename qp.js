769cc6916c1acbc7f17becc0890f0b32ec3fe693
Jacob Lichner <jacob.d.lichner@gmail.com>
2014-10-28 12:32:15 -0700

    Added URI decode

--- qp.js
+++ qp.js
@@ -7,5 +7,5 @@
 
 window.getQueryParameters = function(str) {
   var str = (str || document.location.search);
-  return str == "" ? {} : str.replace(/(^\?)/,'').split("&").map(function(n){return n=n.split("="),this[n[0]]=n[1],this;}.bind({}))[0];
+  return str == "" ? {} : str.replace(/(^\?)/,'').split("&").map(function(n){return n=n.split("="),this[decodeURIComponent(n[0])]=decodeURIComponent(n[1]),this;}.bind({}))[0];
 };
